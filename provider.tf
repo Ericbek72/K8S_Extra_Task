0f9f64fe158033ab3d0ac3ac3b66f8c80214876b
Erkin Bektenov <ericbek72@yahoo.com>
2023-05-19 02:55:00 +0600

    test_commit

--- provider.tf
+++ provider.tf
@@ -1,4 +1,5 @@
 provider "google" {
   project     = "fine-effect-382702"
   region      = "us-central1"
+  credentials = file("fine-effect-382702-3cb2c9adb834.json")
 }
--- service_acc.tf
+++ service_acc.tf
@@ -5,9 +5,22 @@ resource "google_service_account" "service_account" {
 
 resource "google_project_iam_binding" "service_account_binding" {
   project = "fine-effect-382702"
-  role    = "roles/monitoring.alertPolicyEditor"
-
+  #role    = "roles/monitoring.alertPolicyEditor"
+  role    = "roles/monitoring.notificationChannelEditor"
   members = [
     "serviceAccount:${google_service_account.service_account.email}",
   ]
+}
+
+resource "google_project_iam_member" "notification_channel_editor" {
+  project = "fine-effect-382702"
+  role    = "roles/iam.serviceAccountUser"
+  #role    = "roles/monitoring.notificationChannelEditor"
+  member  = "serviceAccount:alerting-sa@fine-effect-382702.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "service_account_permissions" {
+  project = "fine-effect-382702"
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
 }
